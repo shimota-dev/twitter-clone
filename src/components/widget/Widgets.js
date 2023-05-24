59e0e95046a19ecd6e8f3cc157e48f4bce976125
Wataru Shimota <w.shimota@gmail.com>
2023-05-24 15:57:14 +0900

    update css

--- src/components/widget/Widgets.js
+++ src/components/widget/Widgets.js
@@ -1,17 +1,7 @@
-import React from "react";
 import SearchIcon from "@mui/icons-material/Search";
-import {
-	TwitterTimelineEmbed,
-	TwitterShareButton,
-	TwitterFollowButton,
-	TwitterHashtagButton,
-	TwitterMentionButton,
-	TwitterTweetEmbed,
-	TwitterMomentShare,
-	TwitterDMButton,
-	TwitterVideoEmbed,
-	TwitterOnAirButton,
-} from "react-twitter-embed";
+import React from "react";
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterTweetEmbed } from "react-twitter-embed";
+import "./Widgets.css";
 
 function Widgets() {
 	return (
@@ -21,12 +11,13 @@ function Widgets() {
 				<input placeholder="キーワード検索" type="text" />
 			</div>
 
-			<div className="widgets__Container">
+			<div className="widgets__widgetsContainer">
 				<h2>いまどうしてる？</h2>
 				{/* ライブラリ追加 */}
-				<TwitterTweetEmbed tweetID={'1643572400595021824'} />
-				<TwitterTimelineEmbed sourceType="profile" screenName="MikaPikaZo" options={{ height: 400 }} />
-				<TwitterShareButton url={"https://twitter.com/MikaPikaZo"} options={{ text: "#reactjs is awesome", via: "MikaPikaZo" }} />
+				<TwitterTweetEmbed tweetId={"1638797588421025792"} />
+
+				<TwitterTimelineEmbed sourceType="profile" screenName="GitHubJapan" options={{ height: 400 }} />
+				<TwitterShareButton url={"https://twitter.com/GitHubJapan"} options={{ text: "#React.js勉強中", via: "dev_minazuki" }} />
 			</div>
 		</div>
 	);
